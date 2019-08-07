afba49ddabd557907b2cba3098a1a8281b31eae3
Eduardo Hitek <eduardohitek@gmail.com>
2019-08-07 07:02:28 -0300

    Gin

---
+++ bff/game-resource.go
@@ -0,0 +1,86 @@
+package bff
+
+import (
+	"context"
+	"strconv"
+
+	pbgameengine "github.com/eduardohitek/m-apis/m-game-engine/v1"
+	pbhighscore "github.com/eduardohitek/m-apis/m-highscore/v1"
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc"
+)
+
+type gameResource struct {
+	gameClient       pbhighscore.GameClient
+	gameEngineClient pbgameengine.GameEngineClient
+}
+
+func NewGameResource(gameClient pbhighscore.GameClient, gameEngineClient pbgameengine.GameEngineClient) *gameResource {
+	return &gameResource{
+		gameClient:       gameClient,
+		gameEngineClient: gameEngineClient,
+	}
+}
+
+func NewGrpcGameServiceClient(serverAddr string) (pbhighscore.GameClient, error) {
+	conn, err := grpc.Dial(serverAddr, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal().Msgf("Failed to dial: %v", err)
+		return nil, err
+	} else {
+		log.Info().Msgf("Successfully connected to [%s]", serverAddr)
+	}
+
+	if conn == nil {
+		log.Info().Msg("m-highscore connection is nil")
+	}
+
+	client := pbhighscore.NewGameClient(conn)
+
+	return client, nil
+}
+
+func NewGrpcGameEngineServiceClient(serverAddr string) (pbgameengine.GameEngineClient, error) {
+	conn, err := grpc.Dial(serverAddr, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal().Msgf("Failed to dial: %v", err)
+		return nil, err
+	} else {
+		log.Info().Msgf("Successfully connected to [%s]", serverAddr)
+	}
+
+	if conn == nil {
+		log.Info().Msg("m-game-enfine connection is nil")
+	}
+
+	client := pbgameengine.NewGameEngineClient(conn)
+
+	return client, nil
+}
+
+func (gr *gameResource) SetHighScore(c *gin.Context) {
+	highScoreString := c.Param("hs")
+	highScoreFloat64, err := strconv.ParseFloat(highScoreString, 64)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to convert to Float")
+	}
+	gr.gameClient.SetHighScore(context.Background(), &pbhighscore.SetHighScoreRequest{
+		HighScore: highScoreFloat64,
+	})
+}
+
+func (gr *gameResource) GetHighScore(c *gin.Context) {
+
+	highScoreResponse, err := gr.gameClient.GetHighScore(context.Background(), &pbhighscore.GetHighScoreRequest{})
+	if err != nil {
+		log.Error().Err(err).Msg("Error while getting highscore")
+		return
+	}
+	hsString := strconv.FormatFloat(highScoreResponse.HighScore, 'e', -1, 64)
+
+	c.JSONP(200, gin.H{
+		"hs": hsString,
+	})
+
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module github.com/eduardohitek/m-bff
 
 go 1.12
+
+require (
+	github.com/eduardohitek/m-apis v0.0.0-20190807004748-7d4fac9a3f42
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/gin-gonic/gin v1.4.0
+	github.com/json-iterator/go v1.1.7 // indirect
+	github.com/mattn/go-isatty v0.0.8 // indirect
+	github.com/rs/zerolog v1.14.3
+	github.com/stretchr/objx v0.2.0 // indirect
+	github.com/ugorji/go v1.1.7 // indirect
+	golang.org/x/tools v0.0.0-20190806215303-88ddfcebc769 // indirect
+	google.golang.org/grpc v1.22.1
+)
